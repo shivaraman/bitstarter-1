02f77917b7e2e2f74819baa6bfaf68f465dd47c6
Ian S. McBride <ian.s.mcb@gmail.com>
2013-07-15 01:41:57 +0000

    removed default option values for headless grader

--- grader.js
+++ grader.js
@@ -24,9 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+/*
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-
+*/
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -74,9 +75,18 @@ var clone = function(fn) {
 };
 
 if(require.main == module) {
-    program
+/*
+	program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <html_file_url>', 'URL to index.html', clone(assertUrlExists))
+        .parse(process.argv);
+*/
+
+	//removed default values
+    program
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists))
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
 		.option('-u, --url <html_file_url>', 'URL to index.html', clone(assertUrlExists))
         .parse(process.argv);
 
